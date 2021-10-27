94daccc9c66c437631db5e1a26153670d3e214b5
Aso2001001 <83050573+Aso2001001@users.noreply.github.com>
2021-10-27 13:32:36 +0900

    Create 人気.php

---
+++ ソースコード/人気.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div style="text-align: center">
+    <h1>Ishin</h1>
+    <div>
+        <table>
+            <tr>
+                <td><button onclick="location.href='./top (1).php'">TOP</button></td>
+                <td><button onclick="location.href='./search.php'">検索</button></td>
+                <td><button onclick="location.href='./new.php'">新着商品</button></td>
+                <td><button onclick="location.href='/人気.php'">人気商品</button></td>
+                <td><button onclick="location.href='./cart.php'">カート</button></td>
+                <td><button onclick="location.href='./customer.php'">会員情報</button></td>
+            </tr>
+        </table>
+        <h3>当店おすすめのアイテム</h3>
+        <a href="new_women.php">
+            <input type="button" name="women" value="WOMEN">
+        </a>
+
+        <input type="button" name="men" value="MEN"><br>
+
+
+        <a href="new_item_men.php">
+            <input type="image" name="aki" src="img/aki_men.jpeg" alt="秋物メンズパーカー">
+        </a>
+    </div>
+</div>
+
