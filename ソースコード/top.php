97d4e466e4f20601666caab611f93d3c331aed12
Aso2001001 <83050573+Aso2001001@users.noreply.github.com>
2021-10-27 13:21:31 +0900

    Create top.php

---
+++ ソースコード/top.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>top-in</title>
+
+</head>
+<body>
+<div style="text-align: center">
+    <h1>Ishin</h1>
+    <div style="display: inline-table">
+    <table>
+        <tr>
+            <td><button onclick="location.href='検索.php'">検索</button></td>
+            <td><button onclick="location.href='新着.php'">新着商品</button></td>
+            <td><button onclick="location.href='./'">人気商品</button></td>
+            <td><button onclick="location.href='セール.php'">セール</button></td>
+            <td><button onclick="location.href='カート.php'">カート</button></td>
+            <td><button onclick="location.href='会員情報.php'">会員情報</button></td>
+        </tr>
+    </table>
+    </div>
+    <h4>今季新アイテム入荷しました！</h4>
+    <h3>セール開催中</h3>
+    <button>詳しく見る▶︎</button>
+    <h4>おすすめアイテム</h4>
+
+    <tr>
+        <script>
+            function click1() {
+                document.img.src = "./img/women.png";
+            }
+            function click2(){
+                document.img.src = "./img/men.png";
+            }
+        </script>
+        <tb><input type="button" id="women" onclick="click1()" value="WOMEN"></tb>
+        <tb><input type="button" id="men" onclick="click2()" value="MEN"></tb><br>
+        <img name="img" src="./img/women.png">
+    </tr>
+</div>
+
+
+
+
+</body>
+</html>
