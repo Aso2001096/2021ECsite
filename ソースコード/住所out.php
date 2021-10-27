889353f6da729856833d2f4934505c77444dc484
Aso2001001 <83050573+Aso2001001@users.noreply.github.com>
2021-10-27 13:34:37 +0900

    Create 住所out.php

---
+++ ソースコード/住所out.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>login-in</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+<div style="text-align: center">
+<h1>Ishin</h1>
+<p>お届け先情報</p>
+<p>前回のお届け先</p>
+<?php
+echo "以下の情報で登録しました。<br>";
+echo '<p>氏名</p>';
+echo '<p>住所</p>';
+echo '<p>電話番号</p>';
+?>
+<button type="submit" name="action" value="send">この住所に届ける</button>
+</div>
+</body>
+</html>
