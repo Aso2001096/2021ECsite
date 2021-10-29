8c2c00a5c91c0da439c63e3f7770032a0859f63d
Aso2001001 <83050573+Aso2001001@users.noreply.github.com>
2021-10-29 12:36:25 +0900

    Add files via upload

---
+++ ソースコード/passin.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>top-in</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div style="text-align: center">
+    <h1>Ishin</h1>
+    <h3>パスワードお忘れの方</h3>
+    <p>登録のメールアドレスを入力してください</p>
+    <form action="passout.php" method="post">
+        <input type="text" name="email"><br>
+        <button type="submit" name="action" value="send">再設定のメールを送信する</button>
+</div>
+</body>
+</html>
---
+++ ソースコード/passout.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>top-in</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div style="text-align: center">
+    <h1>Ishin</h1>
+    <h3>メールを送信しました</h3>
+    <p>お送りしたメールのURLより<br>パスワードの再設定を行ってください</p>
+    <td><button onclick="location.href='./top (1).php'">page top</button></td>
+</div>
+</body>
+</html>
