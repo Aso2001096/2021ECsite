884e339e401c04614f63417d5806d487461808ab
Aso2001001 <83050573+Aso2001001@users.noreply.github.com>
2021-11-05 11:38:11 +0900

    Update 新着.php

--- ソースコード/新着.php
+++ ソースコード/新着.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="UTF-8">
     <title>新着商品</title>
@@ -9,34 +8,42 @@
 
 <body style="text-align: center">
 
+<h1>Ishin</h1>
+
 <button onclick="location.href='top.php'">TOP</button>
 
 <button onclick="location.href='検索.php'">検索</button>
+
+<button onclick="location.href='./'">人気商品</button>
 
 <button onclick="location.href='セール.php'">セール</button>
 
 <button onclick="location.href='カート.php'">カート</button>
 
-<button onclick="location.href='./'">人気商品</button>
-
 <button onclick="location.href='会員情報.php'">会員情報</button>
 
 <p>新着アイテムページ</p>
 
-<tr>
-    <script>
-        function click1() {
-            document.img.src = "./img/women.png";
-        }
-        function click2(){
-            document.img.src = "./img/men.png";
-        }
-    </script>
-    <tb><input type="button" id="women" onclick="click1()" value="WOMEN"></tb>
-    <tb><input type="button" id="men" onclick="click2()" value="MEN"></tb><br>
-    <img name="img" src="./img/women.png">
-</tr>
-
-
+<table>
+    <tr>
+        <script>
+            function click1() {
+                document.img.src = "./img/women.png";
+                document.getElementById("text").innerHTML = "ワンピース";
+                document.getElementById("text2").innerHTML = "¥1,580";
+            }
+            function click2(){
+                document.img.src = "./img/men.png";
+                document.getElementById("text").innerHTML = "ジャケット";
+                document.getElementById("text2").innerHTML = "¥3,300";
+            }
+        </script>
+        <tb><input type="button" id="women" onclick="click1()" value="WOMEN"></tb>
+        <tb><input type="button" id="men" onclick="click2()" value="MEN"></tb><br>
+        <img name="img" src="./img/women.png">
+    </tr>
+    <tr><p id="text">ワンピース</p></tr>
+    <tr><p id="text2">¥1,580</p></tr>
+</table>
 </body>
 </html>
