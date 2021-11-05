722cda4d0974176236732134a686c07021833a98
Aso2001001 <83050573+Aso2001001@users.noreply.github.com>
2021-11-05 11:37:06 +0900

    Update セール.php

--- ソースコード/セール.php
+++ ソースコード/セール.php
@@ -22,18 +22,27 @@
     </div>
     <br>
     <div style="display: inline-table">
-        <script>
-            function click1() {
-                document.img.src = "./img/women.png";
-            }
-            function click2(){
-                document.img.src = "./img/men.png";
-            }
-        </script>
-        <tb><input type="button" id="women" onclick="click1()" value="WOMEN"></tb>
-        <tb><input type="button" id="men" onclick="click2()" value="MEN"></tb><br>
-        <img name="img" src="./img/women.png">
-    </tr>
+        <table>
+            <tr>
+                <script>
+                    function click1() {
+                        document.img.src = "./img/women.png";
+                        document.getElementById("text").innerHTML = "ワンピース";
+                        document.getElementById("text2").innerHTML = "¥1,580";
+                    }
+                    function click2(){
+                        document.img.src = "./img/men.png";
+                        document.getElementById("text").innerHTML = "ジャケット";
+                        document.getElementById("text2").innerHTML = "¥3,300";
+                    }
+                </script>
+                <tb><input type="button" id="women" onclick="click1()" value="WOMEN"></tb>
+                <tb><input type="button" id="men" onclick="click2()" value="MEN"></tb><br>
+                <img name="img" src="./img/women.png">
+            </tr>
+            <tr><p id="text">ワンピース</p></tr>
+            <tr><p id="text2">¥1,580</p></tr>
+        </table>
 </div>
 </body>
 </html>
