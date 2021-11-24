ba7fa2ffab15635a74536bae9b109b3fc86888a4
Aso2001001 <83050573+Aso2001001@users.noreply.github.com>
2021-11-24 11:53:28 +0900

    Add files via upload

---
+++ ソースコード/注文完了.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>top-in</title>
+</head>
+<body>
+<div style="text-align: center">
+    <h1>Ishin</h1>
+
+    <h1>Thank You!</h1>
+    <p>ご注文ありがとうございます</p>
+    <p>発送準備を行います</p>
+    <p>メールにてご連絡いたしますので<br>しばらくお待ち下さい</p>
+    <button onclick="location.href='top.php'">page top</button>
+</div>
+</body>
+</html>
