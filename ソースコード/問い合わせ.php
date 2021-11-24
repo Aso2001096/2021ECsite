063f4688a329681476d44a659eb0208a599e3767
Aso2001001 <83050573+Aso2001001@users.noreply.github.com>
2021-11-24 11:54:57 +0900

    Add files via upload

---
+++ ソースコード/問い合わせ.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>top-in</title>
+</head>
+<body>
+<div style="text-align: center">
+    <h1>Ishin</h1>
+
+    <p>問い合わせ</p>
+    <button onclick="location.href='top.php'">キャンセル</button>
+    <p>何かご質問ですか？</p>
+    <select name="question">
+        <option value="0">質問の種類を選択してください</option>
+        <option value="1"></option>
+    </select>
+
+    <p>問い合わせ内容</p>
+    <textarea cols="50" rows="10"></textarea><br>
+    <button onclick="location.href='問い合わせ完了.php'">問い合わせる</button>
+</div>
+</body>
+</html>
---
+++ ソースコード/問い合わせ完了.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>top-in</title>
+</head>
+<body>
+<div style="text-align: center">
+    <h1>Ishin</h1>
+    <h1>受け付けました</h1>
+    <p>お時間かかりますが<br>メールにて返答いたします</p>
+    <p>しばらくお待ちください</p>
+    <button onclick="location.href='top.php'">page top</button>
+</div>
+</body>
+</html>
